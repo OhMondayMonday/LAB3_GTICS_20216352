31ee545365a3882e4884f7b0b90c1d8e5c39ddfd
OhMondayMonday <a20216352@pucp.edu.pe>
2025-04-25 20:44:26 -0500

    Initial commit

---
+++ src/main/java/com/example/lab3_gtics_20216352/Lab3Gtics20216352Application.java
@@ -0,0 +1,13 @@
+package com.example.lab3_gtics_20216352;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab3Gtics20216352Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lab3Gtics20216352Application.class, args);
+	}
+
+}
